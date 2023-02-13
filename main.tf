db61cce2fb0f6022beab1bfc2fb53ad0d87bc27d
Abhinav Tripathi <47190942+abhinav-tripathi14@users.noreply.github.com>
2023-02-13 22:02:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "docker_image" "nginx" {
 }
 
 resource "docker_container" "nginx" {
-  image = docker_image.nginx.id
+  image = docker_image.nginx.latest
   name  = "nginx"
   ports {
     internal = 80
